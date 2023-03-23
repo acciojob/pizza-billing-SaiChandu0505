8c26628df56259891b661641980c7422deb5b0a1
SaiChandu0505 <saichandukaligatla017@gmail.com>
2023-03-23 14:37:01 +0530

    Assiginment Third Submit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,16 +3,7 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
 
-    public DeluxePizza(Boolean isVeg, int price, Boolean isVeg1, String bill) {
-        super(isVeg);
-        this.price = price;
-        this.isVeg = isVeg1;
-        this.bill = bill;
-    }
     public DeluxePizza(Boolean isVeg) {
         // your code goes here
         super(isVeg);
@@ -20,29 +11,5 @@ public class DeluxePizza extends Pizza {
         super.addExtraToppings();
 
     }
-    @Override
-    public int getPrice() {
-        return price;
-    }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public Boolean getVeg() {
-        return isVeg;
-    }
-
-    public void setVeg(Boolean veg) {
-        isVeg = veg;
-    }
-
-    @Override
-    public String getBill() {
-        return bill;
-    }
-
-    public void setBill(String bill) {
-        this.bill = bill;
-    }
 }
